176dfcc22d40aae6c510756faa8f88b63c3635bd
Ashiqur Rahaman Dipto <ashikur.r.dipto@gmail.com>
2021-08-07 22:56:48 +0600

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,92 @@
+<html>
+
+<head>
+    <title>CA Project</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <div class="form-title">
+        <p>Registration Form</p>
+    </div>
+
+    <div class="container">
+
+        <form action="" class="from-group">
+
+            <div class="row jumbotron">
+
+                <div class="col-sm-6">
+                    <label for="Name">First Name:</label>
+                    <input type="text" class="form-control" placeholder="First Name:">
+                </div>
+
+                <div class="col-md-6">
+                    <label for="Name">Last Name:</label>
+                    <input type="text" class="form-control" placeholder="Last Name:">
+                </div>
+                <div class="col-md-6">
+                    <label for="Email">Email:</label>
+                    <input type="text" class="form-control" placeholder="example@gmail.com">
+                </div>
+                <div class="col-md-6">
+                    <label for="Phone">Mobile Number:</label>
+                    <input type="number" class="form-control" placeholder="+880">
+                </div>
+                <div class="col-md-6">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" placeholder="***">
+                </div>
+                <div class="col-md-6">
+                    <label for="password">Confirm Password:</label>
+                    <input type="password" class="form-control" placeholder="***">
+                </div>
+
+                <div class="col-md-12">
+                    <label for="nid">NID:</label>
+                    <input type="number" class="form-control" placeholder="NID">
+                </div>
+
+                <div class="col-md-4">
+                    <label for="house">House Number:</label>
+                    <input type="text" class="form-control" placeholder="House No.">
+                </div>
+                <div class="col-md-4">
+                    <label for="road">Road/Block:</label>
+                    <input type="text" class="form-control" placeholder="Road/Block">
+                </div>
+                <div class="col-md-4">
+                    <label for="vill">Village:</label>
+                    <input type="text" class="form-control" placeholder="Village/Area">
+                </div>
+                <div class="col-md-4">
+                    <label for="post">Post Office:</label>
+                    <input type="text" class="form-control" placeholder="Post Office">
+                </div>
+                <div class="col-md-4">
+                    <label for="p.code">Post/Zip Code:</label>
+                    <input type="number" class="form-control" placeholder="Post/Zip Code">
+                </div>
+                <div class="col-md-4">
+                    <label for="vill">Distric:</label>
+                    <input type="text" class="form-control" placeholder="Distric">
+                </div>
+
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+
+
+
+
+        </form>
+
+    </div>
+
+
+
+</html>
